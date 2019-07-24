7ff6998dc5c4ae3099de21224360f63c4c7973d0
Isuru Hettiarachchi <isuru71@gmail.com>
2019-07-24 18:39:24 +0530

    Add methods to scrape search engine results for keyphrase

---
+++ googlescraper/googlescraper.py
@@ -0,0 +1,21 @@
+from GoogleScraper import scrape_with_config, GoogleSearchError
+
+def getUrls(keyword):
+    print(keyword)
+
+    config = {
+        'use_own_ip': 'False',
+        'keyword': keyword,
+        'search_engines': ['bing', ],
+        'num_pages_for_keyword': 1,
+        'scrape_method': 'http',
+        'do_caching': 'False'
+    }
+
+    try:
+        search = scrape_with_config(config)
+    except GoogleSearchError as e:
+        print(e)
+
+    # print(search)
+    return search.serps
